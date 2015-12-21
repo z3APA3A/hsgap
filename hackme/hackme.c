497f507b542d91ac42c237da6d51f2b0b2199025
z3APA3A <3APA3A@3proxy.ru>
2015-12-21 21:46:56 +0300

    Some compatibility between compilers

--- hackme/hackme.c
+++ hackme/hackme.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <process.h>
 
 
 /*
@@ -20,16 +21,21 @@ void GenPass(char * cpPassword, int len){
 	cpPassword[i] = 0;
 }
 
+
+struct user {
+	char cpYear[5] ;
+	char cpPassword[11];
+} u = {"1900", "1234567890"};
+
 int main() {
 	int i, iYear, len;
 	char cpBuf[20];
-	char cpPassword[]="1234567890", cpYear[]="1900";
 
  	InitRnd();   /* Init PRNG */
 
 	for(i=0; i < 3; i++){   /* Give 'im 3 attemps */
 		/* generate new password for each */
-		GenPass(cpPassword, sizeof(cpPassword)); 
+		GenPass(u.cpPassword, sizeof(u.cpPassword)); 
 
 		/* get year of birth */
 		printf(
@@ -48,15 +54,15 @@ int main() {
 
 		/* check he is not scholar */
 		if(iYear >= 2000) {
-			printf("You are a scholar, don't try to fool me\n", iYear);
+			printf("You are a scholar, don't try to fool me\n");
 			return iYear;
 		}
 
 		/* convert year to string */
-		sprintf(cpYear, "%d", iYear);
+		sprintf(u.cpYear, "%d", iYear);
 
 		/* ask for password */
-		printf("OK, dude, born in %s, enter the password: \n", cpYear);
+		printf("OK, dude, born in %s, enter the password: \n", u.cpYear);
 
 		/* zero cpBuf and read new password to it */
 		memset(cpBuf, 0, sizeof(cpBuf));
@@ -67,12 +73,12 @@ int main() {
 			cpBuf[len - 1] = 0;
 
 		/* compare passwords */
-		if (strcmp(cpBuf, cpPassword)){
+		if (strcmp(cpBuf, u.cpPassword)){
 			/* wrong password */
 			printf(
 			 "Nope, dude, your password is '%s', mine is '%s'.\n"
 			 "I will generate new one.\n",
-			 cpBuf, cpPassword
+			 cpBuf, u.cpPassword
 			);
 			continue;
 		}
